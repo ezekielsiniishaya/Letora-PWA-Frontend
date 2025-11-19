f0a3199cea84e1d72a78fd365bb98ab51a5d7386
Ezekiel Ishaya <ezekielsiniishaya@gmail.com>
2025-11-19 22:50:47 +0100

    userType design

--- src/pages/OnboardingPage/ChooseType.jsx
+++ src/pages/OnboardingPage/ChooseType.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Header from "../../components/Header";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 
@@ -11,7 +10,7 @@ export default function ChooseType() {
   const handleProceed = () => {
     if (!selectedRole) {
       setError(true);
-      setTimeout(() => setError(false), 3000); // ⏱ clear error after 5 seconds
+      setTimeout(() => setError(false), 3000);
       return;
     }
     setError(false);
@@ -24,90 +23,114 @@ export default function ChooseType() {
   };
 
   return (
-    <div className="flex flex-col items-center min-h-screen bg-[#F9F9F9] px-6">
-      <Header />
-
+    <div className="flex flex-col items-center min-h-screen bg-[#F9F9F9] px-6 w-full">
       {/* Greeting */}
-      <div className="flex flex-col items-center mt-[41px] text-center">
-        <img
-          src="/icons/greeting.png"
-          alt="Letora Logo"
-          className="w-[95px] h-[95px] mb-[14px]"
-        />
-        <p className="text-[#333333] text-[16px] font-medium">
-          Hello there, What action do
-          <br />
-          you wanna carry out?
-        </p>
+      <div className="w-full max-w-md px-4 mt-[30px]">
+        <div className="flex flex-col items-center mt-[63px] font-medium text-[#333333] text-center">
+          <p className="text-[#333333] text-[16px] font-medium">
+            I am registering as
+          </p>
+          <p className="text-[#666666] text-[13px]">
+            Select one that applies to you
+          </p>
+        </div>
+
+        {/* Buttons */}
+        <div className="flex flex-col space-y-[29px] mt-[45px] w-full">
+          {/* HOST CARD */}
+          <button
+            onClick={() => handleSelect("host")}
+            className="relative flex items-center w-full min-w-full h-[200px] bg-[#21C96B] rounded-[10px] overflow-hidden transition-all duration-200"
+          >
+            {/* faint logo */}
+            <img
+              src="/icons/logo-bg.png"
+              alt="bg-logo"
+              className="absolute right-[-10px] top-[0px] w-[156px]  pointer-events-none"
+            />
+
+            {/* host label text */}
+            <p className="absolute top-[20px] right-[20px] text-white font-semibold text-[18px] z-20">
+              Host
+            </p>
+
+            {/* host image */}
+            <img
+              src="/icons/man.png"
+              alt="Host"
+              className="h-[190px] translate-x-[-20px] translate-y-[8px] relative z-10"
+            />
+
+            {/* HOST CARD selector */}
+            <div
+              className={`absolute right-4 bottom-4 w-[20px] h-[20px] rounded-full flex items-center justify-center ${
+                selectedRole === "host"
+                  ? "border-[4px] border-[#A20BA2] bg-[#FBD0FB]"
+                  : "border-[4px] border-[#A1A1A1] bg-[#D9D9D9]"
+              }`}
+            >
+              {selectedRole === "host" && (
+                <div className="w-[12px] h-[12px] rounded-full bg-[#FBD0FB]" />
+              )}
+            </div>
+          </button>
+
+          {/* GUEST CARD */}
+          <button
+            onClick={() => handleSelect("guest")}
+            className="relative flex items-center w-full min-w-full h-[200px] bg-[#FF703F] rounded-[10px] overflow-hidden transition-all duration-200"
+          >
+            {/* faint logo */}
+            <img
+              src="/icons/logo-bg.png"
+              alt="bg-logo"
+              className="absolute right-[-10px] top-[0px] w-[156px] pointer-events-none"
+            />
+
+            {/* guest label text */}
+            <p className="absolute top-[20px] right-[20px] text-white font-semibold text-[18px] z-20">
+              Guest
+            </p>
+
+            {/* guest image */}
+            <img
+              src="/icons/woman.png"
+              alt="Guest"
+              className="w-[176px] translate-x-[4px] translate-y-1 relative z-10"
+            />
+
+            {/* selector */}
+            <div
+              className={`absolute right-4 bottom-4 w-[20px] h-[20px] rounded-full flex items-center justify-center border-[4px] ${
+                selectedRole === "guest"
+                  ? "border-[4px] border-[#A20BA2] bg-[#FBD0FB]"
+                  : "border-[4px] border-[#A1A1A1] bg-[#D9D9D9]"
+              }`}
+            >
+              {selectedRole === "guest" && (
+                <div className="w-[12px] h-[12px] rounded-full bg-[#FBD0FB]" />
+              )}
+            </div>
+          </button>
+        </div>
       </div>
 
-      {/* Buttons */}
-      <div className="flex flex-col space-y-[29px] mt-[53px] w-full">
-        {/* Host button */}
-        <button
-          onClick={() => handleSelect("host")}
-          className="flex items-center w-full h-[100px] bg-[#DDE9FC] rounded-[5px] overflow-hidden relative transition-all duration-200"
-        >
-          <img
-            src="/icons/host-select.png"
-            alt="Host"
-            className="w-[108px] h-[91px] translate-y-1"
-          />
-          <span className="text-left max-w-[190px] font-medium text-[14px]">
-            I Want to List My Apartment as Shortlet
-          </span>
-
-          <div
-            className={`absolute right-4 top-1/2 -translate-y-1/2 w-[18px] h-[18px] rounded-full flex items-center justify-center border-[4px] ${
-              selectedRole === "host"
-                ? "border-[#A20BA2]"
-                : "border-[#A1A1A1] bg-[#D9D9D9]"
-            }`}
-          >
-            {selectedRole === "host" && (
-              <div className="w-[10px] h-[10px] rounded-full bg-[#FBD0FB]" />
-            )}
-          </div>
-        </button>
-
-        {/* Guest button */}
-        <button
-          onClick={() => handleSelect("guest")}
-          className="flex items-center w-full h-[100px] bg-[#FAB52142] rounded-[5px] overflow-hidden relative transition-all duration-200"
-        >
-          <img
-            src="/icons/guest-select.png"
-            alt="Guest"
-            className="w-[133px] h-full"
-          />
-          <span className="text-left ml-[-25px] w-[181px] font-medium text-[14px]">
-            I Want to Book a Shortlet Apartment
-          </span>
-
-          <div
-            className={`absolute right-4 top-1/2 -translate-y-1/2 w-[18px] h-[18px] rounded-full flex items-center justify-center border-[4px] ${
-              selectedRole === "guest"
-                ? "border-[#A20BA2]"
-                : "border-[#A1A1A1] bg-[#D9D9D9]"
-            }`}
-          >
-            {selectedRole === "guest" && (
-              <div className="w-[10px] h-[10px] rounded-full bg-[#FBD0FB]" />
-            )}
-          </div>
-        </button>
-      </div>
       {/* Error message */}
-      <span className="flex items-left w-full mt-3">
+      <div className="w-full max-w-md mt-3">
         {error && (
           <p className="text-red-500 text-sm font-medium text-left">
             Please select an option to proceed.
           </p>
         )}
-      </span>
+      </div>
+
       {/* Proceed Button */}
-      <div className="mt-[88px] w-full">
-        <Button text="Proceed" onClick={handleProceed} className="mt-4" />
+      <div className="w-full max-w-md mt-[88px]">
+        <Button
+          text="Proceed"
+          onClick={handleProceed}
+          className="mt-4 w-full"
+        />
       </div>
 
       {/* Footer */}
